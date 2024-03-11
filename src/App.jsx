04b81f1c0727ae9e9185191e4729fea2a7c70517
Felipe Gil Gutierrez <felipe.gil@arvato.com>
2024-03-11 11:05:06 +0100

    :frog: First commit :frog:

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>React + Tailwindcss</h1>
+      <div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
